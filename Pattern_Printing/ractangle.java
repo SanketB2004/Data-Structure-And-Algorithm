7e52cfe44fa50259c5a956ee09bb74ab82558037
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-01 04:04:39 +0000

    You Done

---
+++ Pattern_Printing/ractangle.java
@@ -0,0 +1,25 @@
+package Pattern_Printing;
+
+/**
+ * ractangle
+ */
+public class ractangle {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Ractangle Pattern");
+
+        for (int i = 1; i <= 5 ; i++) { // row
+            
+
+            for (int j = 1; j <= 5; j++) { // col
+                
+                System.out.print(" * ");
+            }
+
+             System.out.println();
+        }
+        
+        
+    }
+}
