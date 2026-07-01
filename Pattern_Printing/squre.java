b78419c51421c68910902507e6ed3a6283cf1daa
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-01 04:11:20 +0000

    You Done

---
+++ Pattern_Printing/squre.java
@@ -0,0 +1,31 @@
+package Pattern_Printing;
+
+/**
+ * squre
+ */
+public class squre {
+
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.println("Squre Pattern");
+
+        int a = 5;
+
+        for (int i = 1; i <= a ; i++) { // row
+            
+
+            for (int j = 1; j <= a; j++) { // col
+                
+                System.out.print(" * ");
+            }
+
+             System.out.println();
+        }
+        
+        
+    }
+
+    }
+
