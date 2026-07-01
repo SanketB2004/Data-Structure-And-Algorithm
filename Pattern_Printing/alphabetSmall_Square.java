56c788ed4519b4959fa851c8910dd10a6a77c696
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-01 04:34:55 +0000

    You Done

---
+++ Pattern_Printing/alphabetSmall_Square.java
@@ -0,0 +1,29 @@
+package Pattern_Printing;
+
+public package Pattern_Printing;
+
+/**
+ * alphabet_Square ?? This Is Small  Later
+ */
+public class alphabet_Square {
+
+    public static void main(String[] args) {
+        
+        
+        int a = 5;
+
+        for (int i = 1; i <= a ; i++) { // row
+            
+
+            for (int j = 1; j <= a; j++) { // col
+                
+                System.out.print((char)('a'+j )+" ");
+            }
+
+             System.out.println();
+        }
+    }
+}
+ {
+    
+}
