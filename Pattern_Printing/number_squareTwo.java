42716185b5c6d5d8e417904185575ff9d1d171aa
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-01 04:38:22 +0000

    You Done

---
+++ Pattern_Printing/number_squareTwo.java
@@ -0,0 +1,30 @@
+package Pattern_Printing;
+
+public class number_squareTwo {
+   
+/**
+ * number_Square Second Pattern
+ */
+
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.println("Squre Pattern");
+
+        int a = 5;
+
+        for (int i = 1; i <= a ; i++) { // row
+            
+
+            for (int j = 1; j <= a; j++) { // col
+                
+                System.out.print(i);
+            }
+
+             System.out.println();
+        }
+        
+        
+    }
+} 
