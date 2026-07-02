476aa28cc1d62fd0dedefaf7e4804077d75b747d
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-02 03:38:53 +0000

    You Done

---
+++ Pattern_Printing/star_pattern123456.java
@@ -0,0 +1,32 @@
+package Pattern_Printing;
+
+public class star_pattern123456 {
+   
+
+/**
+ * star_Trangle
+ */
+
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Star  Pattern");
+        int c = 1;
+
+        int a = 5;
+
+        for (int i = 0; i <= a ; i++) { // row
+            
+
+            for (int j = 0; j <= i; j++) { // col
+                
+                System.out.print(c++ +" ");
+            }
+
+             System.out.println();
+        }
+    }
+}
+// 
+
