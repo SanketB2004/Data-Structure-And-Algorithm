e1d970ee6ce40c8f861bd269d0a6d0078d93aabc
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-02 04:57:53 +0000

    You Done

---
+++ diamond.java
@@ -0,0 +1,47 @@
+/**
+ * diamond_pattern
+ */
+public class diamond {
+
+    public static void main(String[] args) {
+System.out.println("Diamond Pattern");
+
+        int n = 4;
+int nsp = n-1, nst = 1;
+        for (int i = 1; i <= n; i++) { // lines
+
+            for (int j = 1; j <=nsp; j++) {//spaces
+                System.out.print("$"+" ");
+            }
+
+            // Stars
+            for (int j = 1; j <= nst; j++) { // stars 
+                System.out.print("*"+" ");
+            }
+            nsp--;
+            nst+=2;
+
+            System.out.println();
+        }
+
+         nsp = 1;
+         nst = 2*n-3;
+        for (int i = 1; i <= n; i++) { // lines
+
+            for (int j = 1; j <=nsp; j++) {//spaces
+                System.out.print("$"+" ");
+            }
+
+            // Stars
+            for (int j = 1; j <= nst; j++) { // stars 
+                System.out.print("*"+" ");
+            }
+            nsp++;
+            nst-=2;
+
+            System.out.println();
+        }
+
+
+    }
+}
