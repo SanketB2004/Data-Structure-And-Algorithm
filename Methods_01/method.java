990f93c15eea70f42c8d368390a22e8c9d9cf48c
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-03 05:07:06 +0000

    Done

---
+++ Methods_01/method.java
@@ -0,0 +1,18 @@
+package Methods_01;
+
+/**
+ * method
+ */
+public class method {
+
+    public static void banau() {
+        System.out.println("Ashirvad ");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sanket");
+        banau();
+        System.out.println("Atharv");
+    }
+
+}
