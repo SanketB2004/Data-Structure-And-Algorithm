2a259ec4ab22d88ad1e15f1a449908858506f212
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-03 04:53:37 +0000

    Done Built In Methods

---
+++ Methods_01/func01.java
@@ -0,0 +1,53 @@
+package Methods_01;
+
+public class func01 {
+    
+    public static void main(String[] args) {
+        
+    //    Bult in Methods . 
+
+    // Math . Bult In Methods 1st CheckOut 
+
+    //  1
+    System.out.println(Math.sqrt(64));
+    // 2 
+    System.out.println(Math.cbrt(64));
+
+    // 3 
+    System.out.println(Math.abs(-85)); // make positive value // readable code make
+
+    // 4 
+
+    System.out.println(Math.floor(6.8));
+
+    //5 
+     System.out.println(Math.ceil(6.8)); // update positive = 7
+//6 
+
+System.out.println(Math.max(9 ,7 )); // maximum
+//7
+System.out.println(Math.min(9 ,7 )); // minimum 
+
+// 8 this is 3 value max
+
+System.out.println(Math.max(Math.max(5, 07),9));//maximum value but three 3 Int
+// 9 find the 4 numbers max
+
+System.out.println(Math.max(Math.max(5, 7), Math.max(7, 9))) ;// Max 4 Values
+   
+
+// 10  power function 
+
+System.out.println(Math.pow(7, 6)); //power function
+
+// 11 
+
+System.out.println((int)(Math.random()*10)+1);
+ 
+
+
+
+
+
+}
+}
