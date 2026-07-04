54260e752499eac05c6d1b6ec173e095a41e70eb
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-04 04:21:22 +0000

    Loading

---
+++ Methods_01/method01Return.java
@@ -0,0 +1,26 @@
+package Methods_01;
+/**
+ * return type method or functions 
+ * check code return  type function code 
+ * this is medimum level methods 
+ * 
+ * 1st methods class are basic function 
+ * 
+ */
+public class method01Return {
+    public static  int presun(){
+
+        System.out.println("Banu");
+        System.out.println("Nanda");
+
+      return 5;
+    }
+
+    public static void main(String[] args) {
+        int x = presun();
+        System.out.println(3+x);
+
+
+    }
+    
+}
