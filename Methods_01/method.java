b7ba62801d608dd8b6868d88f951bd38f6ae5e67
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-04 03:41:22 +0000

    Present

--- Methods_01/method.java
+++ Methods_01/method.java
@@ -7,12 +7,21 @@ public class method {
 
     public static void banau() {
         System.out.println("Ashirvad ");
-    }
+    }// This Not Return
 
     public static void main(String[] args) {
         System.out.println("Sanket");
-        banau();
+         banau(); // call method function
         System.out.println("Atharv");
+
+        banau(); // call method function
+        trishul(); // call method function
     }
 
+
+    public static void trishul(){
+
+        System.out.println("Arpit ");
+
+    }//This Not Return 
 }
