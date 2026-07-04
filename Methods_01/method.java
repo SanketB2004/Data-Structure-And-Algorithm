7787bb440fe48a3a0b5b963a6520084861c706ae
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-04 04:01:08 +0000

    Done

--- Methods_01/method.java
+++ Methods_01/method.java
@@ -5,32 +5,34 @@ package Methods_01;
  */
 public class method {
 
-   public static void sharavan(){
+//    public static void sharavan(){
 
-    karan();
-    System.out.println("Khushi");
+//     karan();
+//     System.out.println("Khushi");
     
-   }
+//    }
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
     
-riyanshi();
+// riyanshi();
+rishika(); // this is enfinide
+
    }
 
-   public static void karan(){
+//    public static void karan(){
 
-    System.out.println("Yashika");
-   }
+//     System.out.println("Yashika");
+//    }
 
-   public static void riyanshi(){
-    karan();;
-    sharavan();
-    System.out.println("Ajay");
-    sharavan();
-   }
+//    public static void riyanshi(){
+//     karan();;
+//     sharavan();
+//     System.out.println("Ajay");
+//     sharavan();
+//    }
 
 
-}
+ // this is methods but use for a method under calling 
 
 // output : 
 // yashika 
@@ -39,3 +41,26 @@ riyanshi();
 // ajay
 // yashika
 // khushi
+
+
+public static void rishika(){
+
+System.out.println("Archit");
+
+ishan();
+}
+
+public static void ishan(){
+    System.out.println("Laksha");
+    varad();
+    
+}
+
+public static void varad(){
+
+    System.out.println("Malani");
+    
+    rishika();
+} // enfinite method 
+
+}
