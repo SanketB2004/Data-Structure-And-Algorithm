3a564f36923cc67a4836cc1e8c2f63cf563e5b55
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-05 05:26:19 +0000

    Commit

---
+++ Arrays/arraybasic01.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+/**
+ * arraybasic
+ */
+
+//This is array basics in a class .// basics - 01
+
+public class arraybasic01 {
+
+
+    public static void main(String[] args) {
+        
+
+        int[] a = new int[20]; // this is array create 1st type 
+
+//         int[] b = {1,2,2,3,4,5,6,7,8,9}; // this is 2nd type array createD
+
+//         a[0] = 5; // 1st method store type method
+
+//         //indexing
+//         System.out.println(b[1]); // acceess of element 
+
+// // update of array element 
+
+//       b[1] = 111;
+
+//       System.out.println(b[1]);
+
+        
+
+        
+    }
+
+    
+
+}
