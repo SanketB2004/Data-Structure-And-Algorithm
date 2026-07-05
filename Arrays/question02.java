107564bcbfee9654123df4b7c167bbda34882426
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-05 06:53:18 +0000

    You Done

---
+++ Arrays/question02.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+// Print sum of array
+
+/**
+ * question02
+ */
+public class question02 {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 4, 45, 55, 89, 7, 5, 556, 88, 88, };
+
+        int n = arr.length;
+        
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+
+             sum = sum + arr[i];
+         
+            
+
+            
+
+        }System.out.print(sum);
+       
+
+    }
+
+}
