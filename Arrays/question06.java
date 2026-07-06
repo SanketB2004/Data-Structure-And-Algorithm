6de59dbc2f4e60dfc1d0265df7cb52471cfceb3e
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-06 07:14:37 +0000

    Change

--- Arrays/question06.java
+++ Arrays/question06.java
@@ -1,23 +1,21 @@
 package Arrays;
 
 public class question06 {
-    // this is a minimum value in array 
+    // this is a minimum value in array
 
     public static void main(String[] args) {
-        
 
-        int[] arr ={54,89,36,45,1,78,88,975};
+        int[] arr = { 54, 89, 36, 45, 1, 78, 88, 975 };
 
         int min = arr[0];
 
         for (int i = 0; i < arr.length; i++) {
-            
-            if (arr[i]< min) {
+
+            if (arr[i] < min) {
 
                 min = arr[i];
-                
-            }
 
+            }
 
         }
         System.out.println(min);
