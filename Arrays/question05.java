f880fa9b819f63982b04dad1b162a6ee52e278cd
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-06 06:27:29 +0000

    done q

--- Arrays/question05.java
+++ Arrays/question05.java
@@ -4,7 +4,29 @@ public class question05 {
     
     public static void main(String[] args) {
         
-        System.out.println("This is question ");
+        System.out.println("Largest Element In Array ");
+
+        // 1st array given 
+
+        int[] arr ={1,8,7,56,90};
+
+        // 2nd print array 
+
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            
+            
+
+            if(largest < arr[i]){
+
+                largest = arr[i];
+            }
+ 
+        }
+System.out.println("Largest Element = " + largest);
+
+
     }
 
 }
