1c1956b0d8b424e4af2c688dd94ca13218c0cf37
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-06 07:38:50 +0000

    Commit

---
+++ Arrays/passingarraytomethod.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class passingarraytomethod {
+    
+    public static void main(String[] args) {
+        
+        int [] x= { 10,29,14,78};
+
+        System.out.println(x[2]);
+
+        change(x);
+
+        System.out.println(x[2]);
+
+    }
+    public static void change(int[]y){
+y[2]=99;
+    }
+}
+
+// this is only array point another variable but variable class not point varible
+// only array point another array 
+// lakshat thev neet imp ahe 
