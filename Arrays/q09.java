f565906cef0f52677fea934e049241d3dcdfa261
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-07 08:02:02 +0000

    commit

---
+++ Arrays/q09.java
@@ -0,0 +1,33 @@
+package Arrays;
+/**
+ * q09
+ * 
+ *  this question is TWO SUM 
+ * 
+ */
+public class q09 {
+
+    public static void main(String[] args) {
+       
+     int[] arr = { 1,5,8,-3};
+
+     int target = 2 ;
+     int num ;
+
+     for (int i = 0; i < arr.length; i++) {
+        
+        for (int j = i+1; j <arr.length; j++) {
+            
+        if((arr[i] + arr[j]) == target){
+
+            System.out.println("Yes The Sum Is target ");
+        } 
+
+
+        }
+        
+     }
+
+    }
+    
+}
