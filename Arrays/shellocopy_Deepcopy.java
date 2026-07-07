2fbb25a070be4435468cacd29e3792693b5484f8
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-07 06:50:16 +0000

    Commit

---
+++ Arrays/shellocopy_Deepcopy.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class shellocopy_Deepcopy {
+    
+
+    public static void main(String[] args) {
+        
+         int[] arr = {10,30,40,50};
+        // int[] x = arr; // x is shallow copy is arr //
+
+        // x[0] = 100 ; // change x to change arr 
+        // System.out.println(arr[0]); // change
+
+      int[] deep = Arrays.copyOf(arr , arr.length); // deep copy 
+ 
+      deep[0] = 500; // change in deep but not change arr
+
+      System.out.println(arr[0]); // not change arr checck and see this its deep copy not ariganal array change
+
+
+      System.out.println(deep[0]); // but deep is change
+ 
+
+        
+    }
+
+
+}
