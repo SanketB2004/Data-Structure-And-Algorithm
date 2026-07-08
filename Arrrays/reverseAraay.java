60073cdf672cbc38023b4005dbcdcb6bac73d260
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-08 08:45:41 +0000

    Commit

---
+++ Arrrays/reverseAraay.java
@@ -0,0 +1,35 @@
+package Arrrays;
+
+public class reverseAraay {
+    public static void main(String[] args) {
+        
+        // Reerace Array 
+
+          int [] arr = {1,2,3,4,5,6,7,8,9};
+
+
+         int n = arr.length;
+
+         int i = 0 , j = arr.length-1;
+
+
+         while (i<j) {
+            int temp = arr[i];
+
+            arr[i]=arr[j];
+
+            arr[j]=temp;
+            i++;
+            j--;
+         }
+
+         for (int ele : arr) {
+            System.out.print(ele);
+         }
+
+
+
+        
+
+    }
+}
