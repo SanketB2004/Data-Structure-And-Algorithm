fc22d2b6fb61800b19c179d6a3e4a8b76633d74b
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-07-08 07:41:47 +0000

    Commit

---
+++ Arrays/SeconsdMax.java
@@ -0,0 +1,31 @@
+// package Arrays;
+
+/**
+ * Second Max
+ */
+public class SeconsdMax {
+
+     public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+
+        int max = arr[0];
+        int secondMax = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
+            }
+            else if (arr[i] > secondMax) {
+                secondMax = arr[i];
+            }
+        }
+
+        System.out.println("Max = " + max);
+        System.out.println("Second Max = " + secondMax);
+    }
+}
+    
+
